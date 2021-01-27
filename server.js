98b6bcda5086858f9d1f80b349fbad4202b8b55e
Craig5117 <craig.bennett5117@gmail.com>
2021-01-26 23:17:04 -0500

    added empty files and set up server

---
+++ server.js
@@ -0,0 +1,46 @@
+const sequelize = require("./config/connection");
+const express = require("express");
+const routes = require("./controllers");
+// const helpers = require("./utils/helpers.js");
+const path = require("path");
+const exphbs = require("express-handlebars");
+// can set defaultLayout in hbs object
+const hbs = exphbs.create({ });
+// const session = require("express-session");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// const sess = {
+//   secret: "aywtd?inshfy?283",
+//   // can set maxAge here
+//   // maxAge: 86400000, for example, would expire the cookie after 24hours
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   // stores the new session to the database exported in config/connection
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.engine("handlebars", hbs.engine);
+// alternately like this:
+// app.engine('handlebars', exphbs({ can set defaultLayout and helpers here}));
+app.set("view engine", "handlebars");
+
+// use session
+// app.use(session(sess));
+// middleware for post requests
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// // use static files in public
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(routes);
+
+// initialize connection to the database then start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
