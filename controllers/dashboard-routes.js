1d48ab4b8597da465d0167c6b2307c62f46b4868
Craig5117 <craig.bennett5117@gmail.com>
2021-01-31 20:19:46 -0500

    added route for students

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,15 +1,55 @@
 const router = require("express").Router();
 // const withAuth = require("../utils/auth");
 const sequelize = require("../config/connection");
-const { User, Student, Goal, StudentGoal } = require('../models');
+const { User, Student, Goal, StudentGoal } = require("../models");
 
-
-router.get('/', (req, res) => {
-    res.render('login');
+router.get("/", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/dashboard")
+    }
+  res.render("login");
 });
 
-router.get('/trial-submit/:id', (req, res) => {
-    res.render('trial-submission');
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/students", (req, res) => {
+  Student.findAll({
+    where: {
+      // this needs to be changed to req.session.user_id
+      user_id: 3,
+    },
+    attributes: ["id", "user_id", "last_name", "first_name"],
+  })
+    .then((studentData) => {
+      const students = studentData.map((student) =>
+        student.get({ plain: true })
+      );
+      // again user_id needs to be replaced with req.session.user_id
+      if (students[0].user_id === 2) {
+        res.render("all-students", { students });
+      } else {
+        res.status(401).render("unauthorized");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/student/:id", (req, res) => {
+    StudentGoal.findAll({
+        where: {
+            student_id: req.params.id,
+        },
+        attributes
+    })
 })
+
+router.get("/trial-submit/:id", (req, res) => {
+  res.render("trial-submission");
+});
 
 module.exports = router;
