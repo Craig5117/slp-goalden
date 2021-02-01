d5e63aae2dfeb84b7160dfa21d9f6d926f2bceca
Craig5117 <craig.bennett5117@gmail.com>
2021-02-01 11:22:34 -0500

    started refactoring routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
 const dashboardRoutes = require('./dashboard-routes.js');
+const apiRoutes = require('./api/');
 const studentRoutes = require('./student-routes.js');
 const goalRoutes = require('./goal-routes.js');
 
 router.use('/', dashboardRoutes);
+router.use('/api', apiRoutes);
 router.use('/students', studentRoutes);
 router.use('/goals', goalRoutes);
 
