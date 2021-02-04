b7300f2045b73f2d576e8c1c9ff16325e3c465fe
Craig5117 <craig.bennett5117@gmail.com>
2021-02-04 00:50:08 -0500

    set force to true temporarily

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 // initialize connection to the database then start the server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
